2e0516b3d848d8153b293100559e4b5a6b3bf8c7
ulwfn123 <251222633@qq.com>
2016-08-01 20:21:00 +0800

    底部菜单的的切换

---
+++ src/cn/ucai/fulicenter/activity/NewGoodFragment.java
@@ -0,0 +1,10 @@
+package cn.ucai.fulicenter.activity;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2016/8/1.
+ */
+public class NewGoodFragment extends Fragment {
+
+}
