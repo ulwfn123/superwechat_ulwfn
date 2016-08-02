4ad8c780b58839edaebbdce0c6a2d946bcfe010a
ulwfn123 <251222633@qq.com>
2016-08-02 10:03:09 +0800

    新品的fragment的布局创建和数据初始化

--- src/cn/ucai/fulicenter/activity/NewGoodFragment.java
+++ src/cn/ucai/fulicenter/activity/NewGoodFragment.java
@@ -1,10 +1,55 @@
 package cn.ucai.fulicenter.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.adapter.GoodAdapter;
+import cn.ucai.fulicenter.bean.NewGoodBean;
 
 /**
  * Created by Administrator on 2016/8/1.
  */
 public class NewGoodFragment extends Fragment {
+    FuliCenterManActivity mContext;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mRecyclerView;
+    GridLayoutManager mGridLayoutManager;
+    GoodAdapter mAdapter;
+    List<NewGoodBean> mGoodList;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mContext = (FuliCenterManActivity) getContext();
+        View layout = View.inflate(mContext, R.layout.fragment_new_good, null);
+        mGoodList = new ArrayList<NewGoodBean>();
+        initView(layout);
+        return  null;
+    }
 
+    private void initView(View layout) {
+        mSwipeRefreshLayout = (SwipeRefreshLayout) layout.findViewById(R.id.srl_new_good);
+        mSwipeRefreshLayout.setColorSchemeColors(
+                R.color.google_blue,R.color.google_yellow,
+                R.color.google_red,R.color.google_green
+        );
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.rv_new_good);
+        mGridLayoutManager = new GridLayoutManager(mContext, I.COLUM_NUM);  //  数据的初始化，括号后的表示每行表示的view个数
+        mGridLayoutManager.setOrientation(LinearLayoutManager.VERTICAL); //  设置线性布局的排列方式
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mAdapter = new GoodAdapter(mContext, mGoodList);
+        mRecyclerView.setAdapter(mAdapter);
+    }
 }
